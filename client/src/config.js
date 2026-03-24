e11197476e018fc98adafbe6dad3bd04f8c44732
Pradeep7007 <pmspradeep007@gmail.com>
2026-03-24 13:17:49 +0530

    error not resolved

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,8 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL;
+let API_BASE_URL = process.env.REACT_APP_API_URL;
+if (!API_BASE_URL || API_BASE_URL === 'undefined') {
+    API_BASE_URL = 'http://localhost:5000';
+} else if (API_BASE_URL.endsWith('/')) {
+    API_BASE_URL = API_BASE_URL.slice(0, -1);
+}
 
 export default API_BASE_URL;
