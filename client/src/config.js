cec9c8a51d5cc2811644cdc9ef4803b9e68e69f3
Pradeep7007 <pmspradeep007@gmail.com>
2026-03-24 11:44:30 +0530

    fix backend

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,5 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
-
+let API_BASE_URL = process.env.REACT_APP_API_URL;
+if (API_BASE_URL && API_BASE_URL.endsWith('/')) {
+    API_BASE_URL = API_BASE_URL.slice(0, -1);
+}
 export default API_BASE_URL;
--- server/index.js
+++ server/index.js
@@ -29,8 +29,4 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-const PORT = process.env.PORT || 5000;
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
 module.exports = app;
