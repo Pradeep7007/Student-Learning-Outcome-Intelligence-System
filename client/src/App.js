96f8f0af12680ab798d8951f4449e365cb8f48db
Pradeep7007 <pmspradeep007@gmail.com>
2026-03-24 09:14:19 +0530

    pages changes

--- client/src/App.js
+++ client/src/App.js
@@ -13,9 +13,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route path="/landing" element={<Landing />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
+        <Route path="/" element={<Signin />} />
         <Route 
           path="/dashboard" 
           element={
